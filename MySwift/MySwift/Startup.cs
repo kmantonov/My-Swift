d4365486aa4aae9eaa355f4f8ab65e148a08a63a
kmantonov <kmantonov@users.noreply.github.com>
2018-04-13 12:02:57 +0300

    Initial commit
    init

---
+++ MySwift/MySwift/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySwift.Startup))]
+namespace MySwift
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
